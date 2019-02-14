c8e6e0341de8f521aa706b1f80a9ab1667ece312
Guillain <guillain@gmail.com>
2019-02-14 18:51:16 +0100

    fix path and add full download

--- classes/gitlab.py
+++ classes/gitlab.py
@@ -23,16 +23,16 @@ class Gitlab(object):
 
     # Standard API methods
     def get(self, path, payload={}):
-        return  self.request("GET", path, payload)
+        return self.request("GET", path, payload)
 
     def put(self, path, payload={}):
-        return  self.request("PUT", path, payload)
+        return self.request("PUT", path, payload)
 
     def post(self, path, payload={}):
-        return  self.request("POST", path, payload)
+        return self.request("POST", path, payload)
 
     def delete(self, path, payload={}):
-        return  self.request("DELETE", path, payload)
+        return self.request("DELETE", path, payload)
 
     # Generic requestor
     def request(self, method, path, payload={}):
@@ -48,8 +48,8 @@ class Gitlab(object):
         return response
 
     # Specific requests
-    def build_url(self, group_name, project_name):
-        return 'https://{}:{}@{}/{}/{}.git'.format(self.user, self.token, self.url, group_name, project_name)
+    def build_url(self, suffix):
+        return 'https://{}:{}@{}/{}.git'.format(self.user, self.token, self.url, suffix)
 
     def get_group(self, group_name):
         for group in self.get("groups"):
@@ -83,8 +83,13 @@ class Gitlab(object):
             repo_project[project['name']] = project['id']
         return repo_project
 
-    def create_project(self, project_name, group_id, visibility="private", request_access_enabled="False"):
+    def get_projects(self, group_id):
+        repo_project = {}
+        for project in self.get("groups/{}/projects".format(group_id)):
+            repo_project[project['name']] = project
+        return repo_project
 
+    def create_project(self, project_name, group_id, visibility="private", request_access_enabled="False"):
         return self.post("projects", {
             "name": project_name,
             "namespace_id": group_id,
@@ -98,8 +103,13 @@ class Gitlab(object):
         except git.GitCommandError as exception:
             print('download project issue', exception)
 
+    def download_projects(self, projects, dir):
+        for project in projects:
+            print("Download project", project, projects[project]['id'], projects[project]['http_url_to_repo'])
+            self.download_project(self.build_url(projects[project]['path_with_namespace']),
+                                  os.path.normpath(os.path.join(dir, projects[project]['name'])))
+
     def sync(self, path_repo, comment, url_remote):
-        project_name = os.path.basename(path_repo)
         repo = git.Repo.init(path_repo)
         res = []
         try:
@@ -146,9 +156,9 @@ class Gitlab(object):
                 projects = self.get_project_ids(group_id)
                 for project_name in projects:
                     project_id = projects[project_name]
-                    self.delete("projects/{}".format(project_id))
+                    self.delete(os.path.join("projects", project_id))
                     print('-', '-', 'Project deleted', project_name, project_id)
-                self.delete("groups/{}".format(group_id))
+                self.delete(os.path.join("groups", group_id))
                 print('-', 'Group deleted')
             else:
                 print('-', 'Group to not delete')
@@ -157,11 +167,11 @@ class Gitlab(object):
     def h_list_project(self, group_name=None):
         group_ids = self.get_group_ids()
         for grp_name in group_ids:
-            project_ids = self.get_project_ids(group_ids[grp_name])
+            projects = self.get_projects(group_ids[grp_name])
             if (group_name not in ('', None) and grp_name == group_name) or group_name in ('', None):
-                print(group_ids[grp_name], grp_name, len(project_ids))
-                for project_name in project_ids:
-                    print('  - ', project_ids[project_name], project_name)
+                print(group_ids[grp_name], grp_name, len(projects))
+                for project in projects:
+                    print('  - ', projects[project]['id'], project, projects[project]['path_with_namespace'])
 
     def h_create_project(self, group_name, project_name):
         group = self.get_group(group_name)
@@ -209,29 +219,37 @@ class Gitlab(object):
                     res = self.sync(
                         os.path.join(path, group_folder, project_name),
                         'Auto-commit',
-                        self.build_url(group_name, project_name)
+                        self.build_url('{}/{}'.format(group_name, project_name))
                     )
                     for action in res:
                         print('-', '-', '-', action)
 
-    def h_download(self, dir, group_name, project_name=''):
+    def h_download(self, dir, group_name='', project_name=''):
 
         if project_name not in ('', None) and group_name not in('', None):
-            self.download_project(self.build_url(group_name, project_name),
+            self.download_project(self.build_url('{}/{}'.format(group_name, project_name)),
                                   os.path.join(dir, group_name, project_name))
             return
 
         if group_name not in('', None):
             group = self.get_group(group_name)
-            if 'id' not in group:
+            if group and group not in('', None) and 'id' not in group:
                 print('Group not found', group_name)
             else:
                 print('Scanning GitLab project...')
-                projects = self.get_project_ids(group['id'])
-                for project_name in projects:
-                    print("Download project", project_name, projects[project_name])
-                    self.download_project(self.build_url(group_name, project_name),
-                                          os.path.join(dir, group_name, project_name))
+                projects = self.get_projects(group['id'])
+                for project in projects:
+                    print("Download project", project, projects[project]['id'], projects[project]['ssh_url_to_repo'])
+                    self.download_project(self.build_url(projects[project]['path_with_namespace']),
+                                          os.path.normpath(os.path.join(dir, group_name, project)))
             return
+        else:
+            group_ids = self.get_group_ids()
+            for grp_name in group_ids:
+                projects = self.get_projects(group_ids[grp_name])
+                for project in projects:
+                    print("Download project", project, projects[project]['id'], projects[project]['ssh_url_to_repo'])
+                    self.download_project(self.build_url(projects[project]['path_with_namespace']),
+                                          os.path.normpath(os.path.join(dir, grp_name, project)))
 
         print('Parameters missing')
